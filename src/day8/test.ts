2db8bbcb70e391ddb140bd46dca8def67e5c10d1
David Heath <david@davidheath.org>
2020-12-12 15:16:24 +0000

    starting

--- src/day8/test.ts
+++ src/day8/test.ts
@@ -1,3 +1,31 @@
-test('basic', () => {
-  expect(0).toBe(0);
-});
+
+let program = `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+`.trim()
+
+function accumulatorValueOnRepetition(program: string): number {
+  return 5
+}
+
+function parse(program: string) {
+  return {op: "nop"}
+}
+
+describe("loop finder", () => {
+  test('basic', () => {
+    expect(accumulatorValueOnRepetition(program)).toBe(5);
+  })
+})
+
+describe("parser", () => {
+  test("it parses the program into instructions", () => {
+    expect(parse("nop +0")).toStrictEqual({op: "nop"})
+  })
+})
